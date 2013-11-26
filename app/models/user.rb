8ef43392e576d76e10789a7c7392be248112d0dd
Scott Luptowski <scottluptowski@gmail.com>
2013-11-26 14:39:42 -0500

    fixed sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,13 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id             :integer          not null, primary key
-#  name           :string(255)
-#  permissions    :integer          default(0)
-#  created_at     :datetime
-#  updated_at     :datetime
-#  uid            :string(255)
-#  github_name    :string(255)
-#  provider       :string(255)
-#  email          :string(255)
-#  image_gravatar :string(255)
-#
-
 class User < ActiveRecord::Base
   has_many :issues
   has_many :responses
 
-  # def self.from_omniauth(auth)
-  #    # where(auth.slice("provider", "uid")).first ||
-
-  #   create_from_omniauth(auth)
-  # end
+  def self.find_or_create_by_omniauth(auth)
+    User.find_by(:provider => auth["provider"], :uid => auth["uid"]) || User.create_from_omniauth(auth)
+  end
 
   def self.create_from_omniauth(auth)
+
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
