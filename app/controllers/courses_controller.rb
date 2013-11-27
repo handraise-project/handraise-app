9faa5dbca54c5b330e3d1163ff7092dcb034bb5e
Scott Luptowski <scottluptowski@gmail.com>
2013-11-27 11:24:01 -0500

    starting courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,37 @@
 class CoursesController < ApplicationController
+  before_filter :set_course, only: [:show, :edit]
+
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to courses_path, :notice => "Created!"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def set_course
+    @course = Course.find_by(:id => params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:name)
+  end
+
 end
