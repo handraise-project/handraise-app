aa99f9daf9fe202106cb3cd25a94a64dc227069a
Anders Ramsay <andersr@gmail.com>
2013-11-27 14:43:58 -0500

    boostrap styling for issue form

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -17,7 +17,7 @@ class Issue < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
   has_many :responses
-  validates_presence_of :title, message: "Issues Must Have A Title."
+  validates_presence_of :title, message: "Please add a title"
 
   def resolved?
     self.resolved > 0 
