dce81e8cb8aa0c72598bc50fa8ca6a8df2507680
Anders Ramsay <andersr@gmail.com>
2013-12-02 13:14:33 -0500

    convert href in issues lists to buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,8 @@ class User < ActiveRecord::Base
     "aviflombaum"
   ]
 
+  
+
   def self.find_or_create_by_omniauth(auth)
     User.find_by(:provider => auth["provider"], :uid => auth["uid"]) || User.create_from_omniauth(auth)
   end
