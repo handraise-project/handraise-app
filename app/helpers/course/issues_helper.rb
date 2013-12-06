aa7687432ae76cef20c760a4952ffd998142632b
Anders Ramsay <andersr@gmail.com>
2013-12-06 18:00:40 -0500

    combined and removed four partials

--- app/helpers/course/issues_helper.rb
+++ app/helpers/course/issues_helper.rb
@@ -14,6 +14,14 @@ module Course::IssuesHelper
     !issue.resolved? && !issue.archived? && (current_user == issue.user || current_user.admin)
   end
 
+  def issue_name(issue)
+    content_tag(:li, "#{issue_poster_name(issue)}", :class => "issue-name")
+  end
+
+  def issue_time_ago(issue)
+    content_tag(:li, "#{time_ago_in_words(issue.created_at, :seconds => true )} ago")
+  end
+
   def issue_name_and_time_ago(issue)
     content_tag(:li, "#{issue_poster_name(issue)}, #{time_ago_in_words(issue.created_at, :seconds => true )} ago")
   end
