28c703e0c2254a7506d43d7cbe42a8035bda5a9a
Anders Ramsay <andersr@gmail.com>
2013-12-07 18:40:23 -0500

    plain text mailer working

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -1,4 +1,6 @@
 class IssueMailer < ActionMailer::Base
+  include ActionView::Helpers::SanitizeHelper
+
   default from: "help@iqueue.co"
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -9,11 +11,19 @@ class IssueMailer < ActionMailer::Base
   def new_reply_notification(issue, response, current_user)
     @course_name_truncated = issue.course.name.split[0...2].join(' ')
     @issue_title = issue.title
-    @response_content = response.description
-    @recipient_email = current_user.email
-    @recipient_first_name = current_user.name.split[0]
+    @response_content_html = response.description
+    @response_content_plain_text = strip_tags(response.description)
+    @recipient = User.find(issue.user_id)
+    @recipient_email = @recipient.email
+    @recipient_first_name = @recipient.name.split[0]
     @respondent = User.find(response.user_id)
-    @respondent_name = @respondent.name
+    
+    if @respondent.admin
+      @respondent_name = "#{@respondent.name} (Instructor)"
+    else
+      @respondent_name = @respondent.name
+    end
+    
     @issue_link = course_issue_url(issue.course, issue)
 
     mail from: "Iqueue Notices <notices@iqueue.co>", to: "#{@recipient_email}", subject: "[#{@course_name_truncated}] New Reply to '#{@issue_title}'", :reply_to => "help@iqueue.co"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,22 +16,21 @@ Handraise::Application.configure do
 # ActionMailer Settings
 config.action_mailer.default_url_options = { :host => '127.0.0.1:3000' }
 
-config.action_mailer.delivery_method = :letter_opener
+# config.action_mailer.delivery_method = :letter_opener
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
 
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default :charset => "utf-8"
-
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: "gmail.com",
-  #   authentication: "plain",
-  #   user_name: ENV["DEVMAIL_USERNAME"],
-  #   password: ENV["DEVMAIL_PASSWORD"]
-  #   #enable_starttls_auto: true
-  # }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: ENV["DEVMAIL_USERNAME"],
+    password: ENV["DEVMAIL_PASSWORD"]
+    #enable_starttls_auto: true
+  }
 
 
 
