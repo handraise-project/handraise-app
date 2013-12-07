87322d1172f05a078008e09b9f9317471be1cdda
Anders Ramsay <andersr@gmail.com>
2013-12-07 18:53:31 -0500

    added missing host config value

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Handraise::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'iqueue.co' }
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
